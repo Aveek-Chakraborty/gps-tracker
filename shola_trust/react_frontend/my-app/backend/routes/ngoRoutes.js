8eb15841911ae596032caf73769d1e5980096c63
Akash Kumar <ak9608450671@gmail.com>
2025-06-12 16:11:18 +0530

    Delete shola_trust directory

--- shola_trust/react_frontend/my-app/backend/routes/ngoRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { getNGOs } = require('../controllers/ngoController');
-const router = express.Router();
-
-router.get('/ngos', getNGOs);
-
-module.exports = router;
